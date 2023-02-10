0f9ac351713f15ef1961533e5e88737e8c9ca375
source <mom.sombrathna26@gmail.com>
2023-02-10 21:58:42 +0700

    fdsg

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json, user.json');
+const router = jsonServer.router('db.json');
+const user = jsonServer.router('user.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
+server.use(user);
 
 server.listen(port);
